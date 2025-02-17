0073b8e5d2856a730e145ee4b15c7a67931377f8
Jane Onwanya <123064279+maryjjjannee@users.noreply.github.com>
2025-02-17 18:23:56 +0700

    Add initial HTML structure and integrate Morgan for logging

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const chalk = require('chalk');
-const debug = require('debug')('app:startup');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
 
 const app = express();
 const port = 3100;
 
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, '/public/')));
+
 app.get('/', (req, res) => {
-    res.send('Hello World! This is my first Node.js app');
+    res.send('Hello World! This is my first Node.js appdd');
 });
 
 app.listen(port, () => {
