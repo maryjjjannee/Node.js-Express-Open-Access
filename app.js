beeedccd04d8bcd2a14d2996ca6e6f6f2ace5440
Jane Onwanya <123064279+maryjjjannee@users.noreply.github.com>
2025-02-18 16:02:48 +0700

    Refactor app.js to implement product routing and improve code formatting

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-const express = require('express');
-const chalk = require('chalk');
-const debug = require('debug')('app');
-const morgan = require('morgan');
-const path = require('path');
-
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const path = require("path");
+const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname, '/public/')));
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-
-    res.render('index', {username: "Onwanya" , customer: ["Doja", "Harry", "Lisa"]});
+productRouter.route("/").get((req, res) => {
+  res.send("Hello from product");
 });
 
+productRouter.route("/").get((req, res) => {
+  res.send("Hello from product 1");
+});
+
+app.use("/products", productRouter);
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    username: "Onwanya",
+    customer: ["Doja", "Harry", "Lisa"],
+  });
+});
 
 app.listen(PORT, () => {
-    debug("Listening on port " + chalk.green(" : " + PORT));
+  debug("Listening on port " + chalk.green(" : " + PORT));
 });
 
 module.exports = app;
