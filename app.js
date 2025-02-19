2f8adb3674d3e2f2917b66be1899159f99bab7b8
Jane Onwanya <123064279+maryjjjannee@users.noreply.github.com>
2025-02-19 19:43:11 +0700

    Add product data JSON file and update product routing to use external data

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
-const productRouter = express.Router();
+const products = require("./data/products.json");
+const productRouter = express.Router()
 
 const app = express();
 const PORT = process.env.PORT;
@@ -15,14 +16,9 @@ app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
 
 productRouter.route("/").get((req, res) => {
-  res.render("products",{
-    products: [
-      {productTitle: "Lipstick 01", productDescription: "Red Rosy Mauve", productPrice: 399},
-      {productTitle: "Lipstick 02", productDescription: "NudePink", productPrice: 399},
-      {productTitle: "Lipstick 03", productDescription: "Orange Marmalade", productPrice: 399},
-      {productTitle: "Lipstick 04", productDescription: "Natural Clear", productPrice: 399},
-    ],
-  });
+  res.render("products",
+    products,
+  );
 });
 
 productRouter.route("/").get((req, res) => {
