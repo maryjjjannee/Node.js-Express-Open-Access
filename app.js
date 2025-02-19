1dc945011229d78fcee24fc3bf5fad44bfa3edaf
Jane Onwanya <123064279+maryjjjannee@users.noreply.github.com>
2025-02-19 20:07:53 +0700

    Refactor product routing and add product data JSON file

--- app.js
+++ app.js
@@ -3,35 +3,17 @@ const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
-const products = require("./data/products.json");
-const productRouter = express.Router()
 
 const app = express();
 const PORT = process.env.PORT;
+const productsRouter = require("./src/views/router/productsRouter");
 
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
 
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
-
-productRouter.route("/").get((req, res) => {
-  res.render("products",{
-    products,
-  }
-  );
-});
-
-productRouter.route("/:id").get((req, res) => {
-  const id = req.params.id;
-  res.render("product",
-    {
-      product: products[id],
-    }
-  );
-});
-
-app.use("/products", productRouter);
+app.use("/products", productsRouter);
 
 app.get("/", (req, res) => {
   res.render("index", {
